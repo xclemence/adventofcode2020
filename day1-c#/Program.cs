76620ed0938083b507d3c5ce0a8fb0d7471b2f62
Xavier CLEMENCE <xavier.clemence@gmail.com>
2021-01-31 13:18:19 +0100

    Day 1 - C#

---
+++ day1-c#/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var noteString = await File.ReadAllLinesAsync(@".\notes.txt");
+
+var notes = noteString.Select(x => int.Parse(x)).ToArray();
+
+Part1();
+Part2();
+
+void Part1()
+{
+    foreach (var item in notes)
+    {
+        var corresponding = notes.FirstOrDefault(x => x + item == 2020);
+
+        if (corresponding != 0)
+        {
+            Console.WriteLine($"{item} - {corresponding}");
+            return;
+        }
+    }
+}
+
+void Part2()
+{
+    for (var i = 0; i < notes.Length; ++i)
+    {
+        for (var j = i + 1; j < notes.Length; ++j)
+        {
+            var corresponding = notes.Skip(j).FirstOrDefault(x => x + notes[i] + notes[j] == 2020);
+
+            if (corresponding != 0)
+            {
+                Console.WriteLine($"{notes[i]} - {notes[j]} - {corresponding}");
+                return;
+            }
+        }
+    }
+
+}
+
